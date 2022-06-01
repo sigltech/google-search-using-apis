881a4fb11c1a8c5925ac93e97c9abd4213b764ed
sigltech <101203107+sigltech@users.noreply.github.com>
2022-06-01 09:17:25 +0100

    index.js has script tags added now

--- server/app.js
+++ server/app.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const data = require('./data');
 const app = express();
-
-app.use(cors);
-
-app.listen(3000, (req, res) => {
-    console.log('You are running on port 3000!')
-});
+let port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello from the dark side!')
+});
+
+app.listen(port, (req, res) => {
+    console.log(`You are running on port ${port}!`)
 });
 
